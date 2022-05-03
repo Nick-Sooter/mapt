49718d72a31345e617d7fc4438802a52f220f41e
Nick Sooter <nick.sooter@gmail.com>
2022-05-03 16:22:03 -0600

    bcrypt installed for password hashing

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const User = require('../models/User');
+const bcrypt = require('bcrypt');
 
 // login
 
@@ -7,8 +8,16 @@ const User = require('../models/User');
 
 
 // register
+// Have to be careful here - schema calls for a password field, but if password is listed, it will be shown in database (not secure)
+// install bcrypt - library to help hash passwords
+// require bcrypt above
 router.pot('/register', async (req, res) => {
   try {
+    // generate new password
+
+    // create new user
+
+    // save user and send response
 
   } catch (err) {
     res.status(500).json(err)
